62fc280ac9ed8472986df5b7477c94c207989dd6
孤独的蔬菜 <shuai19970217@163.com>
2017-05-18 09:22:44 +0800

    结构

--- 5.18_lx/Program.cs
+++ 5.18_lx/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace _5._18_lx
 {
+
+    //定义结构
+    struct Person {
+        string name;
+        int age;
+        double height;
+        string sex;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            #region 
+
+            #endregion
         }
     }
 }
